4816b61c6b73a755fe65860e7ce045e668d4ec8a
Arafatullah Arshad <arafatullaharshad@gmail.com>
2026-06-25 23:56:38 +0600

    fix

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://resell-hub-nine.vercel.app",
 });
 
 export const {
